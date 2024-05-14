961db593293b739efeb7122a54f7c3f7503fceb0
CrispyFig <kmwh030@gmail.com>
2024-05-14 15:32:33 +0900

    [feat] nav 기능 추가

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    var aboutmeNav = document.getElementById("aboutmeNav");
+    var skillsNav = document.getElementById("skillsNav");
+    var logNav = document.getElementById("logNav");
+    var aboutmeWapper = document.getElementById("aboutme-wrapper");
+    var skillsWapper = document.getElementById("skills-wrapper");
+    var logWapper = document.getElementById("log-wrapper");
+
+    aboutmeNav.addEventListener('click', function() {
+        aboutmeWapper.style.display = 'block';
+        skillsWapper.style.display = 'none';
+        logWapper.style.display = 'none';
+    });
+
+    skillsNav.addEventListener('click', function() {
+        aboutmeWapper.style.display = 'none';
+        skillsWapper.style.display = 'block';
+        logWapper.style.display = 'none';
+    });
+
+    logNav.addEventListener('click', function() {
+        aboutmeWapper.style.display = 'none';
+        skillsWapper.style.display = 'none';
+        logWapper.style.display = 'block';
+    });
+}
