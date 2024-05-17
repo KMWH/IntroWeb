0202f40c5a5e6306ba494b2ba28e689e0917c315
KMWH <kmwh030@gmail.com>
2024-05-17 13:16:37 +0900

    [feat] 디자인 수정

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-window.onload = function() {
-    var aboutmeNav = document.getElementById("aboutmeNav");
-    var skillsNav = document.getElementById("skillsNav");
-    var logNav = document.getElementById("logNav");
-    var aboutmeWapper = document.getElementById("aboutme-wrapper");
-    var skillsWapper = document.getElementById("skills-wrapper");
-    var logWapper = document.getElementById("log-wrapper");
+window.onload = function () {
+    var aboutmeNav = document.getElementById('aboutmeNav');
+    var skillsNav = document.getElementById('skillsNav');
+    var logNav = document.getElementById('logNav');
+    var aboutmeWapper = document.getElementById('aboutme-wrapper');
+    var skillsWapper = document.getElementById('skills-wrapper');
+    var logWapper = document.getElementById('log-wrapper');
 
-    aboutmeNav.addEventListener('click', function() {
-        aboutmeWapper.style.display = 'block';
+    aboutmeNav.addEventListener('click', function () {
+        aboutmeWapper.style.display = 'flex';
         skillsWapper.style.display = 'none';
         logWapper.style.display = 'none';
     });
 
-    skillsNav.addEventListener('click', function() {
+    skillsNav.addEventListener('click', function () {
         aboutmeWapper.style.display = 'none';
-        skillsWapper.style.display = 'block';
+        skillsWapper.style.display = 'flex';
         logWapper.style.display = 'none';
     });
 
-    logNav.addEventListener('click', function() {
+    logNav.addEventListener('click', function () {
         aboutmeWapper.style.display = 'none';
         skillsWapper.style.display = 'none';
-        logWapper.style.display = 'block';
+        logWapper.style.display = 'flex';
     });
-}
+};
