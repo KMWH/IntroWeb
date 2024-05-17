4163e31cf195f471613fd4debe5f76cd03d92294
KMWH <kmwh030@gmail.com>
2024-05-17 17:42:17 +0900

    [feat] nav hover 기능 추가

--- script.js
+++ script.js
@@ -6,19 +6,30 @@ window.onload = function () {
     var skillsWapper = document.getElementById('skills-wrapper');
     var logWapper = document.getElementById('log-wrapper');
 
+    // aboutmeNav.style.color = 'white';
+
     aboutmeNav.addEventListener('click', function () {
+        // aboutmeNav.style.color = 'white';
+        // skillsNav.style.color = 'rgb(175, 175, 175)';
+        // logNav.style.color = 'rgb(175, 175, 175)';
         aboutmeWapper.style.display = 'flex';
         skillsWapper.style.display = 'none';
         logWapper.style.display = 'none';
     });
 
     skillsNav.addEventListener('click', function () {
+        // aboutmeNav.style.color = 'rgb(175, 175, 175)';
+        // skillsNav.style.color = 'white';
+        // logNav.style.color = 'rgb(175, 175, 175)';
         aboutmeWapper.style.display = 'none';
         skillsWapper.style.display = 'flex';
         logWapper.style.display = 'none';
     });
 
     logNav.addEventListener('click', function () {
+        // aboutmeNav.style.color = 'rgb(175, 175, 175)';
+        // skillsNav.style.color = 'rgb(175, 175, 175)';
+        // logNav.style.color = 'white';
         aboutmeWapper.style.display = 'none';
         skillsWapper.style.display = 'none';
         logWapper.style.display = 'flex';
