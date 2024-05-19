40b4f7276abe6d95474a57c4e3a0674bbd0d0dd4
CrispyFig <kmwh030@gmail.com>
2024-05-19 22:46:08 +0900

    [feat] aboutme 모바일 화면 추가

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
 window.onload = function () {
-    var aboutmeNav = document.getElementById('aboutmeNav');
-    var skillsNav = document.getElementById('skillsNav');
-    var logNav = document.getElementById('logNav');
-    var aboutmeWapper = document.getElementById('aboutme-wrapper');
-    var skillsWapper = document.getElementById('skills-wrapper');
-    var logWapper = document.getElementById('log-wrapper');
+    var links = document.querySelectorAll('.nav-link');
+    var sections = document.querySelectorAll('.wrapper');
 
-    // aboutmeNav.style.color = 'white';
+    function showSection(sectionId) {
+        sections.forEach(section => {
+            section.style.display = (section.id === sectionId) ? 'flex' : 'none';
+        });
+    }
 
-    aboutmeNav.addEventListener('click', function () {
-        // aboutmeNav.style.color = 'white';
-        // skillsNav.style.color = 'rgb(175, 175, 175)';
-        // logNav.style.color = 'rgb(175, 175, 175)';
-        aboutmeWapper.style.display = 'flex';
-        skillsWapper.style.display = 'none';
-        logWapper.style.display = 'none';
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            links.forEach(l => l.classList.remove('active'));
+            link.classList.add('active');
+
+            const target = link.getAttribute('data-target');
+            showSection(target);
+        });
     });
 
-    skillsNav.addEventListener('click', function () {
-        // aboutmeNav.style.color = 'rgb(175, 175, 175)';
-        // skillsNav.style.color = 'white';
-        // logNav.style.color = 'rgb(175, 175, 175)';
-        aboutmeWapper.style.display = 'none';
-        skillsWapper.style.display = 'flex';
-        logWapper.style.display = 'none';
-    });
-
-    logNav.addEventListener('click', function () {
-        // aboutmeNav.style.color = 'rgb(175, 175, 175)';
-        // skillsNav.style.color = 'rgb(175, 175, 175)';
-        // logNav.style.color = 'white';
-        aboutmeWapper.style.display = 'none';
-        skillsWapper.style.display = 'none';
-        logWapper.style.display = 'flex';
-    });
+    links[0].classList.add('active');
+    showSection(links[0].getAttribute('data-target'));
 };
